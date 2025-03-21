210cbbec8dbb0e742ed836c7e72dacb1c97122f8
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2025-03-22 02:28:01 +0600

    link routing enabled with smooth scroll and active button functionality implemented

--- scripts/Category.js
+++ scripts/Category.js
@@ -10,13 +10,28 @@ const showCategory = (categories) => {
         const div = document.createElement("div");
         div.innerHTML = `
         <button onclick = "loadPetsByCatgory('${element.category}')"
-        class = "text-lg btn py-6 px-10 rounded-3xl hover:bg-[#0E7A8119 ]">
+        class = "select-btn text-lg btn py-6 px-10 rounded-3xl hover:bg-[#0E7A8119 ]">
         <img class="w-8" src = "${element.category_icon}" alt = "">
         ${element.category}
         </button>
         `
         categoryContainer.appendChild(div);
     });
+
+    const selectCategory = document.getElementsByClassName("select-btn");
+    for (const selectBtn of selectCategory) {
+        selectBtn.addEventListener("click", function () {
+            removeActiveClass();
+            selectBtn.classList.add("active", "bg-[#0E7A81]", "text-white");
+        })
+    }
+}
+
+const removeActiveClass = () => {
+    const activeBtn = document.getElementsByClassName("active");
+    for (const btn of activeBtn) {
+        btn.classList.remove("active", "bg-[#0E7A81]", "text-white");
+    }
 }
 
 loadCategories();
